7fcbd2ac5dafcbef4e6ed4859a24bd3d7c998fa1
Dennis Hernández <dennishernandezvargas@gmail.com>
2016-02-18 14:01:14 -0600

    Adding more functionality

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -8,10 +8,24 @@ var condominio = $("#condominio"),
                 .text($('<div />').html(condominios[i]).text()));
         }
     },
+    tipoPropiedad = function () {
+        $("#ingPropiedad input[name='tipoalq']").click(function(){
+            if($('input:radio[name=tipoalq]:checked').val() === "casa"){
+                $("#areacomp").hide();
+                $(".cuotaseg").show();
+                $(".cuotamant").hide();
+            } else {
+                $("#areacomp").show();
+                $(".cuotaseg").hide();
+                $(".cuotamant").show();
+            }
+        });
+    }
     init = function () {
         setTimeout(function () {
             cargarCondominios();
         }, 250);
+        tipoPropiedad();
     };
 
 init();
