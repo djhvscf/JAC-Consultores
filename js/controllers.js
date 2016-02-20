b6355b913944b277291c5a429ed5047e9d4c311f
Dennis Hernández <dennishernandezvargas@gmail.com>
2016-02-19 23:59:32 -0600

    Api for Propiedad

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,9 @@
 var jacconsultoresControllers = angular.module('jacconsultoresControllers', []);
 
-jacconsultoresControllers.controller('propiedadCtrl', function ($scope) {
+jacconsultoresControllers.controller('propiedadCtrl', function ($scope, $http) {
     var that = this;
+
+    $scope.formPropiedad = {};
 
     $scope.isCondominio = true;
     $scope.isCasaResindencial = false;
@@ -10,10 +12,100 @@ jacconsultoresControllers.controller('propiedadCtrl', function ($scope) {
         $scope.isCondominio = name === "casa" ? false : true;
         $scope.isCasaResindencial = name === "casa" ? true : false;
     };
+
+    /**
+     * Condominos
+     */
+    $http.get('/api/condominios')
+        .success(function(data) {
+            $scope.condominios = data;
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    $scope.createCondominio = function(){
+        $http.post('/api/condominios', {nombre: "Tramonto"})
+            .success(function(data) {
+                $scope.formData = {};
+                $scope.condominios = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error:' + data);
+            });
+    };
+
+    //$scope.createCondominio();
+    /**
+     * Condominos
+     */
+
+    /**
+     * Propiedades
+     */
+
+    /*$http.get('/api/propiedad')
+        .success(function(data) {
+            $scope.propiedades = data;
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });*/
+
+    $scope.createPropiedad= function(){
+        $http.post('/api/propiedad', $scope.formPropiedad)
+            .success(function(data) {
+                $scope.formPropiedad = {};
+                $scope.propiedades = data;
+            })
+            .error(function(data) {
+                console.log('Error:' + data);
+            });
+    };
+    /**
+     * Propiedades
+     */
 });
 
-jacconsultoresControllers.controller('loteCtrl', function ($scope) {
+jacconsultoresControllers.controller('loteCtrl', function ($scope, $http) {
     var that = this;
+    $scope.formData = {text: "str"};
+
+    // Cuando se cargue la página, pide del API todos los TODOs
+    $http.get('/api/todos')
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data)
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // Cuando se añade un nuevo TODO, manda el texto a la API
+    $scope.createTodo = function(){
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {};
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error:' + data);
+            });
+    };
+
+    // Borra un TODO despues de checkearlo como acabado
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error:' + data);
+            });
+    };
 });
 
 jacconsultoresControllers.controller('clienteCtrl', function ($scope) {
---
+++ server.js
@@ -0,0 +1,210 @@
+var express     = require('express');
+var app         = express();
+var mongoose     = require('mongoose');
+
+// Conexión con la base de datos
+mongoose.connect('mongodb://localhost:27017/jacconsultores');
+
+// Configuración
+app.configure(function() {
+    // Localización de los ficheros estÃ¡ticos
+    app.use(express.static(__dirname + '/'));
+    // Muestra un log de todos los request en la consola
+    app.use(express.logger('dev'));
+    // Permite cambiar el HTML con el método POST
+    app.use(express.bodyParser());
+    // Simula DELETE y PUT
+    app.use(express.methodOverride());
+});
+
+var Todo = mongoose.model('Todo', {
+    text: String
+});
+
+/*****************************************************************
+ * Condominios API
+ ****************************************************************/
+var Condominio = mongoose.model("Condominio", {
+    nombre: String
+});
+
+app.get('/api/condominios', function(req, res) {
+    Condominio.find(function(err, condominios) {
+        if(err) {
+            res.send(err);
+        }
+        res.json(condominios);
+    });
+});
+
+app.post('/api/condominios', function(req, res) {
+    Condominio.create({
+        nombre: req.body.nombre
+    }, function(err, condominio){
+        if(err) {
+            res.send(err);
+        }
+
+        Condominio.find(function(err, condominios) {
+            if(err){
+                res.send(err);
+            }
+            res.json(condominios);
+        });
+    });
+});
+/*****************************************************************
+ * Condominios API
+ ****************************************************************/
+
+/*****************************************************************
+ * Propiedad API
+ ****************************************************************/
+var Propiedad = mongoose.model("Propiedad", {
+    isVenta: Boolean,
+    direccion: String,
+    isPropietario: Boolean,
+    telefono: String,
+    comision: Number,
+    cuotaMant: Number,
+    cuotaSeg: Number,
+    lote: Number,
+    const: Number,
+    anos: Number,
+    niveles: Number,
+    garage: Number,
+    habitaciones: Number,
+    banos: Number,
+    oficina: Boolean,
+    terra: Boolean,
+    patio: Boolean,
+    sala: Boolean,
+    anteComedor: Boolean,
+    ctoServ: Boolean,
+    salaTv: Boolean,
+    cocina: Boolean,
+    ctoPilas: Boolean,
+    bodega: Boolean,
+    otros: String,
+    areaComp: String,
+    observaciones: String
+});
+
+// Rutas de nuestro API
+// GET de todos los TODOs
+app.get('/api/propiedad', function(req, res) {
+    Propiedad.find(function(err, propiedades) {
+        if(err) {
+            res.send(err);
+        }
+        res.json(propiedades);
+    });
+});
+
+// POST que crea un TODO y devuelve todos tras la creación
+app.post('/api/propiedad', function(req, res) {
+    Propiedad.create({
+        isVenta: req.body.isVenta,
+        direccion: req.body.direccion,
+        isPropietario: req.body.isPropietario,
+        telefono: req.body.telefono,
+        comision: req.body.comision,
+        cuotaMant: req.body.cuotaMant,
+        cuotaSeg: req.body.cuotaSeg,
+        lote: req.body.lote,
+        const: req.body.const,
+        anos: req.body.anos,
+        niveles: req.body.niveles,
+        garage: req.body.garage,
+        habitaciones: req.body.habitaciones,
+        banos: req.body.banos,
+        oficina: req.body.oficina,
+        terra: req.body.terra,
+        patio: req.body.patio,
+        sala: req.body.sala,
+        anteComedor: req.body.anteComedor,
+        ctoServ: req.body.ctoServ,
+        salaTv: req.body.salaTv,
+        cocina: req.body.cocina,
+        ctoPilas: req.body.ctoPilas,
+        bodega: req.body.bodega,
+        otros: req.body.otros,
+        areaComp: req.body.areaComp,
+        observaciones: req.body.observaciones
+    }, function(err, propiedad){
+        if(err) {
+            res.send(err);
+        }
+
+        Propiedad.find(function(err, propiedades) {
+            if(err){
+                res.send(err);
+            }
+            res.json(propiedades);
+        });
+    });
+});
+/*****************************************************************
+ * Propiedad API
+ ****************************************************************/
+
+
+// Rutas de nuestro API
+// GET de todos los TODOs
+app.get('/api/todos', function(req, res) {
+    Todo.find(function(err, todos) {
+        if(err) {
+            res.send(err);
+        }
+        res.json(todos);
+    });
+});
+
+// POST que crea un TODO y devuelve todos tras la creación
+app.post('/api/todos', function(req, res) {
+    Todo.create({
+        text: req.body.text,
+        done: false
+    }, function(err, todo){
+        if(err) {
+            res.send(err);
+        }
+
+        Todo.find(function(err, todos) {
+            if(err){
+                res.send(err);
+            }
+            res.json(todos);
+        });
+    });
+});
+
+// DELETE un TODO específico y devuelve todos tras borrarlo.
+app.delete('/api/todos/:todo', function(req, res) {
+    Todo.remove({
+        _id: req.params.todo
+    }, function(err, todo) {
+        if(err){
+            res.send(err);
+        }
+
+        Todo.find(function(err, todos) {
+            if(err){
+                res.send(err);
+            }
+            res.json(todos);
+        });
+
+    })
+});
+
+// Carga una vista HTML simple donde irá nuestra Single App Page
+// Angular Manejará el Frontend
+app.get('*', function(req, res) {
+    res.sendfile('index.html');
+});
+
+// Escucha en el puerto 8080 y corre el server
+app.listen(8080, function() {
+    console.log('App listening on port 8080');
+});
